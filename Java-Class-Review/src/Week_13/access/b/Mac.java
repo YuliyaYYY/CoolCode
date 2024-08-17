f5f0bc2daa48a54cf1584f61eeeeb9562b5a3367
Julia <yuliya_nc@yahoo.com>
2024-08-17 13:22:24 -0700

    Mac, RunnerB classes added

---
+++ Java-Class-Review/src/Week_13/access/b/Mac.java
@@ -0,0 +1,17 @@
+package Week_13.access.b;
+
+import Week_13.access.a.Computer;
+
+public class Mac extends Computer {
+
+    public String toString(){
+        return "Macbook Pro{" +
+                "os='" + os + '\'' +
+                ", memory=" + memory +
+                ", brand='" + brand +
+                '}';
+
+
+    }
+
+}
---
+++ Java-Class-Review/src/Week_13/access/b/RunnerB.java
@@ -0,0 +1,11 @@
+package Week_13.access.b;
+
+public class RunnerB {
+
+    public static void main(String[] args) {
+
+        Mac mac = new Mac();
+        mac.os = "Sonoma";
+      //  mac.memory = 32;
+    }
+}
